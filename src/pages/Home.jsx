d2bfe6ca766969282f0acd61cdb5e8720f7a8a94
nagma <nagma@navankur.org>
2025-01-24 11:38:01 +0530

    create a home page box beacuase i learn a github how to work..?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div className='h-[60vh] w-full flex justify-center items-center text-5xl font-bold'>Home</div>
-    )
-}
+  return (
+    <div className="h-[60vh] w-full flex justify-center items-center text-5xl font-bold">
+      <div className="w-52 h-52 bg-slate-400 text-4xl font-medium flex items-center justify-center">Home</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
